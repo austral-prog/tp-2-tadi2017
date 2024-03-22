77989aeef9fa16f7f83d916976916af21180d7a0
tadi2017 <162723256+tadi2017@users.noreply.github.com>
2024-03-22 12:57:18 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,9 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    full_name = f"{first_name} {last_name}"
+    print(full_name.lower())
+    print(full_name.title())
+    print(full_name.upper())
+    print(f"    {first_name.lower()} {last_name.lower()}")
+ada() 
