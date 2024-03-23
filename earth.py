03168ddb4bd31c6d8c515bee2b448c0b8a47d12d
tadi2017 <162723256+tadi2017@users.noreply.github.com>
2024-03-23 08:58:34 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,3 +1,8 @@
 def earth():
     x = "Bangladesh"
     y = "Barbados"
+    Q = (x < y)
+    S = (y < x)
+    print(f"The result of {x} comes first in the dictionary is {Q}")
+    print(f"The result of {y} comes first in the dictionary is {S}")
+earth()
