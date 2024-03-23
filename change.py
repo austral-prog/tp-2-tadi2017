66347fc41f158ff873369e6d8f56c6bd617f156f
tadi2017 <162723256+tadi2017@users.noreply.github.com>
2024-03-23 08:57:58 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,3 +1,9 @@
 def change():
     expense = 23.75
     money = 100
+    x = int(money - expense)
+    y = float((money - expense)% 1)
+    print("\nVuelto\n")
+    print(f"Pesos:\n{x}")
+    print(f"Centavos:\n{y}")
+change()         
